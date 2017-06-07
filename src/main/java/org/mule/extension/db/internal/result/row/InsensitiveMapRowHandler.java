53c1957a8206b5df81554db31b3ae2e2db36d351
Pablo Kraan <pablokraan@users.noreply.github.com>
2017-06-07 23:23:15 +0200

    MULE-12609: Every exported Mule package must contain api (#15)

--- src/main/java/org/mule/extension/db/internal/result/row/InsensitiveMapRowHandler.java
+++ src/main/java/org/mule/extension/db/internal/result/row/InsensitiveMapRowHandler.java
@@ -7,7 +7,7 @@
 
 package org.mule.extension.db.internal.result.row;
 
-import org.mule.runtime.core.util.CaseInsensitiveHashMap;
+import org.mule.runtime.core.api.util.CaseInsensitiveHashMap;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;

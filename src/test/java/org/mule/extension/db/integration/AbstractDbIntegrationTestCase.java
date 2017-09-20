7c0b096a37844a270a1bcd3195576b47d4b73d23
estebanwasinger <estebanwasinger@gmail.com>
2017-09-20 16:39:06 -0300

    MULE-13568: None and Pooling connection providers doesn't use the Plugin ClassLoader to create connections - Fix Injection

--- src/test/java/org/mule/extension/db/integration/AbstractDbIntegrationTestCase.java
+++ src/test/java/org/mule/extension/db/integration/AbstractDbIntegrationTestCase.java
@@ -22,7 +22,7 @@ import static org.mule.extension.db.integration.TestRecordUtil.assertRecords;
 import static org.mule.metadata.api.model.MetadataFormat.JAVA;
 import static org.mule.runtime.api.component.location.Location.builder;
 import static org.mule.runtime.api.metadata.MetadataKeyBuilder.newKey;
-import static org.mule.runtime.core.internal.connection.util.ConnectionProviderUtils.unwrapProviderWrapper;
+import static org.mule.runtime.core.api.connection.util.ConnectionProviderUtils.unwrapProviderWrapper;
 
 import org.mule.extension.db.api.StatementResult;
 import org.mule.extension.db.integration.model.AbstractTestDatabase;

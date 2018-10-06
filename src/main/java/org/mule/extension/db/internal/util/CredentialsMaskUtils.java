837c50a8b7f5cd0ec2108ccc735a243b71c9a0cf
bardammu <juan.baca@mulesoft.com>
2018-10-05 23:32:53 -0300

    MULE-15793: Can't get stored procedure name from sql query when there are whitespaces before parentheses

--- src/main/java/org/mule/extension/db/internal/util/CredentialsMaskUtils.java
+++ src/main/java/org/mule/extension/db/internal/util/CredentialsMaskUtils.java
@@ -1,11 +1,10 @@
-package org.mule.extension.db.internal.util;
-
 /*
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+package org.mule.extension.db.internal.util;
 
 import static java.lang.String.format;
 import static java.util.regex.Pattern.compile;
---
+++ src/main/java/org/mule/extension/db/internal/util/StoredProcedureUtils.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.db.internal.util;
+
+import static java.lang.String.format;
+
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StoredProcedureUtils {
+
+  private final static Pattern storedProcedureMatcher = Pattern.compile("(?msi)(\\{\\s+)?call\\s* \\s*(\\w+)\\s*\\(.*");
+
+  public static String getStoredProcedureName(String sqlText) throws SQLException {
+    Matcher matcher = storedProcedureMatcher.matcher(sqlText);
+
+    if (!matcher.matches()) {
+      throw new SQLException(format("Unable to detect stored procedure name from '%s'", sqlText));
+    }
+
+    return matcher.group(2);
+  }
+
+}

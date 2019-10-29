c9426fb658d4f5a82bd70aee15dd439854d7ef91
Sebastian Elizalde <sebi.elizalde@gmail.com>
2019-10-29 13:47:38 -0300

    MULE-17204: Consider upgrading the suggested mysql driver. (#196)

--- src/test/java/org/mule/extension/db/integration/model/MySqlTestDatabase.java
+++ src/test/java/org/mule/extension/db/integration/model/MySqlTestDatabase.java
@@ -197,8 +197,9 @@ public class MySqlTestDatabase extends AbstractTestDatabase {
   public void createDelayFunction(DataSource dataSource) throws SQLException {
     executeDdl(dataSource, "DROP FUNCTION IF EXISTS DELAY;\n");
 
-    final String sql = "CREATE FUNCTION DELAY(seconds INTEGER) RETURNS INTEGER\n" + "BEGIN\n" + " DO SLEEP(seconds * 1000);\n"
-        + " RETURN 1;\n" + "END;";
+    final String sql =
+        "CREATE FUNCTION DELAY(seconds INTEGER) RETURNS INTEGER DETERMINISTIC\n" + "BEGIN\n" + " DO SLEEP(seconds * 1000);\n"
+            + " RETURN 1;\n" + "END;";
     createStoredProcedure(dataSource, sql);
   }
 

8a40db87ea8b0fae1a0d79f740addaed2dee98ad
Juan Baca Paunero <wencesj@gmail.com>
2020-02-03 18:30:50 -0300

    MULE-17966: DB Connector tests must use dynamic ports

--- src/test/java/org/mule/extension/db/integration/connectivity/DataSourcePoolingMySqlTestCase.java
+++ src/test/java/org/mule/extension/db/integration/connectivity/DataSourcePoolingMySqlTestCase.java
@@ -6,24 +6,26 @@
  */
 package org.mule.extension.db.integration.connectivity;
 
-import org.junit.Before;
-import org.junit.runners.Parameterized;
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static org.mule.extension.db.integration.TestDbConfig.getMySqlResource;
 
 import java.util.List;
 
-import static org.mule.extension.db.integration.TestDbConfig.getMySqlResource;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
 
 public class DataSourcePoolingMySqlTestCase extends DataSourcePoolingTestCase {
 
   private static final String DB_HOST_KEY = "db.host";
   private static final String DB_HOST_VALUE = "0.0.0.0";
   private static final String DB_PORT_KEY = "db.port";
-  private static final String DB_PORT_VALUE = "3306";
+  private static final String DB_PORT_VALUE = getProperty("mysql.db.port");
 
   @Before
   public void setUp() {
-    System.setProperty(DB_HOST_KEY, DB_HOST_VALUE);
-    System.setProperty(DB_PORT_KEY, DB_PORT_VALUE);
+    setProperty(DB_HOST_KEY, DB_HOST_VALUE);
+    setProperty(DB_PORT_KEY, DB_PORT_VALUE);
     setConcurrentRequests(2);
   }
 
--- src/test/java/org/mule/extension/db/integration/connectivity/DataSourcePoolingOracleTestCase.java
+++ src/test/java/org/mule/extension/db/integration/connectivity/DataSourcePoolingOracleTestCase.java
@@ -6,24 +6,26 @@
  */
 package org.mule.extension.db.integration.connectivity;
 
-import org.junit.Before;
-import org.junit.runners.Parameterized;
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static org.mule.extension.db.integration.TestDbConfig.getOracleResource;
 
 import java.util.List;
 
-import static org.mule.extension.db.integration.TestDbConfig.getOracleResource;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
 
 public class DataSourcePoolingOracleTestCase extends DataSourcePoolingTestCase {
 
   private static final String DB_HOST_KEY = "db.host";
   private static final String DB_HOST_VALUE = "0.0.0.0";
   private static final String DB_PORT_KEY = "db.port";
-  private static final String DB_PORT_VALUE = "1521";
+  private static final String DB_PORT_VALUE = getProperty("oracle.db.port");
 
   @Before
   public void setUp() {
-    System.setProperty(DB_HOST_KEY, DB_HOST_VALUE);
-    System.setProperty(DB_PORT_KEY, DB_PORT_VALUE);
+    setProperty(DB_HOST_KEY, DB_HOST_VALUE);
+    setProperty(DB_PORT_KEY, DB_PORT_VALUE);
     setConcurrentRequests(2);
   }
 

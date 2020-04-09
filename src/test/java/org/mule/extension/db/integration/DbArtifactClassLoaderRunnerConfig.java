6538d5a7284beac949122816cdc658958f9543ec
mmariotti <mmariotti@mariotti-ltm.internal.salesforce.com>
2020-04-09 00:08:42 -0300

    MULE-18290:Bulk Operation Error throws UNKNOWN exception.

--- src/test/java/org/mule/extension/db/integration/DbArtifactClassLoaderRunnerConfig.java
+++ src/test/java/org/mule/extension/db/integration/DbArtifactClassLoaderRunnerConfig.java
@@ -19,6 +19,6 @@ import org.mule.test.runner.ArtifactClassLoaderRunnerConfig;
 @ArtifactClassLoaderRunnerConfig(
     exportPluginClasses = {DbConnectionProvider.class, StatementStreamingResultSetCloser.class},
     applicationSharedRuntimeLibs = {"org.apache.derby:derby", "mysql:mysql-connector-java", "com.microsoft.sqlserver:mssql-jdbc",
-        "com.oracle:ojdbc8"})
+        "com.oracle.database.jdbc:ojdbc8"})
 public interface DbArtifactClassLoaderRunnerConfig {
 }

570fdbdac30c77ce573ffa5a608d46b50595f3f1
Euge <emariotti3@users.noreply.github.com>
2020-04-24 09:23:17 -0300

    MULE-18260: Error on BulkInsert within a For Each: can't cast from TypedValue to java.util.Map (#246)
    (cherry picked from commit a3cb8cc03c5ff0436cc9d57aea829f4db4090e5d)

--- src/test/java/org/mule/extension/db/integration/insert/BulkInsertTestCase.java
+++ src/test/java/org/mule/extension/db/integration/insert/BulkInsertTestCase.java
@@ -11,12 +11,15 @@ import static java.sql.Statement.SUCCESS_NO_INFO;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.mule.extension.db.integration.TestRecordUtil.assertRecords;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.AnyOf.anyOf;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.rules.ExpectedException.none;
 import org.mule.extension.db.integration.AbstractDbIntegrationTestCase;
+import org.mule.extension.db.integration.model.Field;
+import org.mule.extension.db.integration.model.Record;
 import org.mule.runtime.api.message.Message;
 
 import java.sql.SQLException;
@@ -24,12 +27,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class BulkInsertTestCase extends AbstractDbIntegrationTestCase {
+
+  private static int POSITION_OFFSET_1 = 300;
+  private static String NAME_FIELD = "NAME";
+  private static List<String> PLANET_NAMES =
+      Arrays.asList("Vogsphere", "Caprica", "Coruscant", "Worlorn", "Ego", "Krypton", "Gallifrey",
+                    "Cybertron", "Dagobah", "Terra Prime");
 
   @Rule
   public ExpectedException expectedException = none();
@@ -59,10 +69,38 @@ public class BulkInsertTestCase extends AbstractDbIntegrationTestCase {
     flowRunner("bulkInsertWithUnusedParameterType").withPayload(values()).run().getMessage();
   }
 
+  @Test
+  public void bulkInsertInsideForEachScope() throws Exception {
+    Message response =
+        flowRunner("bulkInsertInsideForEachScope").withPayload(valuesWithIncrementalPosition(PLANET_NAMES, POSITION_OFFSET_1))
+            .withVariable("positionOffset", POSITION_OFFSET_1).keepStreamsOpen().run()
+            .getMessage();
+
+    assertRecords(response.getPayload().getValue(),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(0))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(1))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(2))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(3))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(4))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(5))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(6))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(7))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(8))),
+                  new Record(new Field(NAME_FIELD, PLANET_NAMES.get(9))));
+  }
+
   private List<Map<String, Object>> values() {
     List<Map<String, Object>> values = new ArrayList<>();
     addRecord(values, "Pluto", 777);
     addRecord(values, "Saturn", 777);
+    return values;
+  }
+
+  private List<Map<String, Object>> valuesWithIncrementalPosition(List<String> planetNames, int offset) {
+    List<Map<String, Object>> values = new ArrayList<>();
+    for (int i = 0; i < planetNames.size(); i++) {
+      addRecord(values, planetNames.get(i), offset + i);
+    }
     return values;
   }
 

d61b5e5ddf805cf86f0c11e13b532d36d6bef059
Julian Diaz <59581166+julodiaz@users.noreply.github.com>
2020-09-29 14:42:40 -0300

    changes info to debug

--- src/main/java/org/mule/extension/db/internal/domain/executor/BulkUpdateExecutor.java
+++ src/main/java/org/mule/extension/db/internal/domain/executor/BulkUpdateExecutor.java
@@ -122,7 +122,7 @@ public class BulkUpdateExecutor extends AbstractExecutor implements BulkExecutor
           .error("BULK UPDATE EXCEPTION. DATABASE PROCESSED {} OPERATIONS SUCCESSFULLY AND STOPPED PROCESSING DUE TO EXCEPTION.",
                  successfulOperations + noInfoAvailable);
     } else {
-      LOGGER.info("SUCCESSFULLY EXECUTED BATCH OPERATION. TOTAL EXECUTED STATEMENTS: {}.", batchCount);
+      LOGGER.debug("SUCCESSFULLY EXECUTED BATCH OPERATION. TOTAL EXECUTED STATEMENTS: {}.", batchCount);
     }
   }
 }

03477fb51a7c7b595cc15f7ecddf856083149962
julian.diaz <julian.diaz@mulesoft.com>
2020-09-29 12:35:23 -0300

    fix/DBCON-225: adds tests

---
+++ src/test/java/org/mule/extension/db/integration/storedprocedure/StoredProcedureReturningConnectionTestCase.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.extension.db.integration.storedprocedure;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mule.extension.db.integration.AbstractDbIntegrationTestCase;
+import org.mule.runtime.api.message.Message;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+
+public class StoredProcedureReturningConnectionTestCase extends AbstractDbIntegrationTestCase {
+
+  public static final String CYCLED_SUCCESSFULLY = "CYCLED SUCCESSFULLY";
+  public static final String CONNECTIVITY = "CONNECTIVITY";
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Override
+  protected String[] getFlowConfigurationResources() {
+    return new String[] {"integration/storedprocedure/stored-procedure-returning-connections.xml"};
+  }
+
+  @Before
+  public void setupStoredProcedure() throws Exception {
+    testDatabase.createStoredProcedureParameterizedUpdatePlanetDescription(getDefaultDataSource());
+  }
+
+  @Test
+  public void convertsStringToClob() throws Exception {
+    Message response = flowRunner("storedProcedureReturnsConnectionsWhenNoStreamNorTransaction")
+        .withPayload(TEST_MESSAGE).run().getMessage();
+    assertThat(response.getPayload().getValue(), equalTo(CYCLED_SUCCESSFULLY));
+    assertThat(response.getPayload().getValue(), not(CONNECTIVITY));
+  }
+
+}

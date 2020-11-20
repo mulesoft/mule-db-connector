9aec9c8b89d2ddfe3f74cf73d142e8361641009a
julian.diaz <julian.diaz@mulesoft.com>
2020-11-20 14:37:54 -0300

    fix/DBCON-234: replaces InsensitiveMapRowHandler with NonStreamingInsensitiveMapRowHandler to read into memory any Stream since the Listener does not provide any mechanism to keep track of them (thus closing the connection and prohibiting the consumption of Stream in any processor).

--- src/main/java/org/mule/extension/db/internal/source/RowListener.java
+++ src/main/java/org/mule/extension/db/internal/source/RowListener.java
@@ -21,11 +21,10 @@ import org.mule.extension.db.internal.resolver.query.ParameterizedQueryResolver;
 import org.mule.extension.db.internal.resolver.query.QueryResolver;
 import org.mule.extension.db.internal.result.resultset.ListResultSetHandler;
 import org.mule.extension.db.internal.result.resultset.ResultSetHandler;
-import org.mule.extension.db.internal.result.row.InsensitiveMapRowHandler;
+import org.mule.extension.db.internal.result.row.NonStreamingInsensitiveMapRowHandler;
 import org.mule.runtime.api.connection.ConnectionException;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.api.exception.MuleException;
-import org.mule.runtime.api.lifecycle.Initialisable;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.metadata.MetadataKeyId;
 import org.mule.runtime.extension.api.annotation.metadata.MetadataScope;
@@ -198,7 +197,7 @@ public class RowListener extends PollingSource<Map<String, Object>, Void> {
       statementFactory.setQueryTimeout(new Long(settings.getQueryTimeoutUnit().toSeconds(settings.getQueryTimeout())).intValue());
 
       ResultSetHandler resultSetHandler =
-          new ListResultSetHandler(new InsensitiveMapRowHandler(connection, Charset.forName(encoding)));
+          new ListResultSetHandler(new NonStreamingInsensitiveMapRowHandler(connection, Charset.forName(encoding)));
 
       List<Map<String, Object>> rows =
           (List<Map<String, Object>>) new SelectExecutor(statementFactory, resultSetHandler).execute(connection, query);

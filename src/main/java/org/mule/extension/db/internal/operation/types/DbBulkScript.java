7e9b9eb2a0a3e24bfb5d5c66cf9ca22ecf5f39b9
Julian Diaz <julian.diaz@salesforce.com>
2021-03-24 11:09:23 -0300

    feature/DBCON-245: adds header

--- src/main/java/org/mule/extension/db/internal/operation/types/DbBulkScript.java
+++ src/main/java/org/mule/extension/db/internal/operation/types/DbBulkScript.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.extension.db.internal.operation.types;
 
 import org.mule.db.commons.api.param.BulkScript;

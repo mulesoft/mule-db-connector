42b5024be92dbfe79adfe2827d09dbb3df7d79b9
Julian Diaz <julian.diaz@salesforce.com>
2021-06-03 15:17:47 -0300

    fix/DBCON-278: leaves original pair and adds recent ones, this will probably grow as users report missing pairs

---
+++ src/main/java/org/mule/extension/db/internal/domain/connection/oracle/types/OracleOlderXMLType.java
@@ -0,0 +1,12 @@
+package org.mule.extension.db.internal.domain.connection.oracle.types;
+
+public class OracleOlderXMLType extends OracleXmlTypeHandler {
+
+    private static final int OPAQUE_TYPE_ID = 2007;
+    private static final String XML_TYPE_NAME = "XMLTYPE";
+
+    public OracleOlderXMLType() {
+        super(OPAQUE_TYPE_ID, XML_TYPE_NAME);
+    }
+
+}
---
+++ src/main/java/org/mule/extension/db/internal/domain/connection/oracle/types/OracleOpaqueXMLType.java
@@ -0,0 +1,12 @@
+package org.mule.extension.db.internal.domain.connection.oracle.types;
+
+public class OracleOpaqueXMLType extends OracleXmlTypeHandler {
+
+    private static final int OPAQUEXML_TYPE_ID = 2009;
+    private static final String OPAQUEXML_TYPE_NAME = "OPAQUE/XMLTYPE";
+
+    public OracleOpaqueXMLType() {
+        super(OPAQUEXML_TYPE_ID, OPAQUEXML_TYPE_NAME);
+    }
+
+}

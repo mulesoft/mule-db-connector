b864ec55f1da03014b414a3d190e4fa0efd8b590
Julian Diaz <julian.diaz@salesforce.com>
2021-06-03 15:17:56 -0300

    fix/DBCON-278: leaves original pair and adds recent ones, this will probably grow as users report missing pairs

---
+++ src/main/java/org/mule/extension/db/internal/domain/connection/oracle/types/OracleXMLType.java
@@ -0,0 +1,12 @@
+package org.mule.extension.db.internal.domain.connection.oracle.types;
+
+public class OracleXMLType extends OracleXmlTypeHandler {
+
+    private static final int XMLTYPE_TYPE_ID = 2009;
+    private static final String XMLTYPE_TYPE_NAME = "XMLTYPE";
+
+    public OracleXMLType() {
+        super(XMLTYPE_TYPE_ID, XMLTYPE_TYPE_NAME);
+    }
+
+}

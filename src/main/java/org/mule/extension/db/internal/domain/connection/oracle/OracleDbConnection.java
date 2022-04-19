d032fe460163dccf16bae16571f4567e6c44d74e
Nicolás Lichtmaier <nlichtmaier@mulesoft.com>
2022-04-19 12:09:39 -0300

    OracleDbConnetion.getProcedureColumns(): Only pass "" as catalog if catalogName is null (it always will be, but this looks saner)

--- src/main/java/org/mule/extension/db/internal/domain/connection/oracle/OracleDbConnection.java
+++ src/main/java/org/mule/extension/db/internal/domain/connection/oracle/OracleDbConnection.java
@@ -259,7 +259,7 @@ public class OracleDbConnection extends DefaultDbConnection {
       procedureColumns = dbMetaData.getProcedureColumns(storedProcedureOwner, connectionSchema, storedProcedureName, "%");
       if (!procedureColumns.isBeforeFirst()) {
         procedureColumns.close();
-        procedureColumns = dbMetaData.getProcedureColumns("", storedProcedureOwner, storedProcedureName, "%");
+        procedureColumns = dbMetaData.getProcedureColumns(catalogName == null ? "" : catalogName, storedProcedureOwner, storedProcedureName, "%");
       }
     } else {
       procedureColumns = dbMetaData.getProcedureColumns(catalogName, connectionSchema, storedProcedureName, "%");

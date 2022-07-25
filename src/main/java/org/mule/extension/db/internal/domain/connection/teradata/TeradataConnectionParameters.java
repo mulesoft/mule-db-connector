93fb39042d525aa4dbea5ec0b4f967aab438dc54
Tan Nguyen <dev.tannguyen@gmail.com>
2022-07-24 19:59:31 -0500

    Add Teradata connection files

---
+++ src/main/java/org/mule/extension/db/internal/domain/connection/teradata/TeradataConnectionParameters.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.db.internal.domain.connection.teradata;
+
+import org.mule.db.commons.internal.domain.connection.generic.GenericConnectionProvider;
+import org.mule.extension.db.internal.domain.connection.BaseDbConnectionParameters;
+import org.mule.extension.db.internal.domain.connection.DataSourceConfig;
+import org.mule.runtime.extension.api.annotation.param.Optional;
+import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.ClassValue;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Password;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
+
+/**
+ *  {@link DataSourceConfig}'s parameters for the {@link GenericConnectionProvider}
+ *
+ * @since 1.0
+ *  @deprecated since 1.9.4. Replace with equivalent on mule-db-client. To be removed in the next major (2.0).
+ */
+@Deprecated
+public class TeradataConnectionParameters extends BaseDbConnectionParameters implements DataSourceConfig {
+
+  /**
+   * JDBC URL to use to connect to the database.
+   */
+  @Parameter
+  @Placement(order = 1)
+  @DisplayName("URL")
+  private String url;
+
+  /**
+   * Fully-qualified name of the database driver class.
+   */
+  @Parameter
+  @Placement(order = 2)
+  @ClassValue(extendsOrImplements = "java.sql.Driver")
+  private String driverClassName;
+
+  /**
+   * Database username
+   */
+  @Parameter
+  @Placement(order = 3)
+  @Optional
+  private String user;
+
+  /**
+   * Database password
+   */
+  @Parameter
+  @Placement(order = 4)
+  @Password
+  @Optional
+  private String password;
+
+  @Override
+  public String getUrl() {
+    return url;
+  }
+
+  @Override
+  public String getDriverClassName() {
+    return driverClassName;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUser() {
+    return user;
+  }
+
+}
---
+++ src/main/java/org/mule/extension/db/internal/domain/connection/teradata/TeradataConnectionProvider.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.db.internal.domain.connection.teradata;
+
+import org.mule.db.commons.internal.domain.connection.DbConnection;
+import org.mule.db.commons.internal.domain.connection.generic.GenericConnectionProvider;
+import org.mule.db.commons.internal.domain.type.ResolvedDbType;
+import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.ExternalLib;
+import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+
+import java.sql.Connection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.mule.db.commons.internal.domain.connection.DbConnectionProvider.DRIVER_FILE_NAME_PATTERN;
+import static org.mule.runtime.api.meta.ExternalLibraryType.JAR;
+
+/**
+ * {@link ConnectionProvider} that creates connections for any kind of database using a JDBC URL
+ * and the required JDBC Driver Class
+ */
+@DisplayName("Teradata Connection")
+@Alias("teradata")
+@ExternalLib(name = "JDBC Driver", description = "A JDBC driver which supports connecting to the Database",
+    nameRegexpMatcher = DRIVER_FILE_NAME_PATTERN, type = JAR)
+public class TeradataConnectionProvider extends GenericConnectionProvider {
+
+  private final Map<String, Map<Integer, ResolvedDbType>> resolvedDbTypesCache = new ConcurrentHashMap<>();
+
+  @Override
+  protected DbConnection createDbConnection(Connection connection) throws Exception {
+    return super.createDbConnection(connection);
+  }
+
+}

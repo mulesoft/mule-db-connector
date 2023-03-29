77e5e5f49d335228e6839aee0532ec911ff63d32
GuidoGianfrate <ggianfrate@mulesoft.com>
2023-03-29 10:35:59 -0300

    Fix test

---
+++ src/test/java/org/mule/extension/db/integration/storedprocedure/oracle/StoredProcedureReturningOutputBlobTestCase.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.extension.db.integration.storedprocedure.oracle;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.mule.extension.db.integration.AbstractDbIntegrationTestCase;
+import org.mule.extension.db.integration.model.OracleTestDatabase;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.mule.extension.db.integration.TestDbConfig.getOracleResource;
+
+public class StoredProcedureReturningOutputBlobTestCase extends AbstractDbIntegrationTestCase {
+
+  @Parameterized.Parameters(name = "{2}")
+  public static List<Object[]> parameters() {
+    List<Object[]> params = new LinkedList<>();
+
+    if (!getOracleResource().isEmpty()) {
+      final OracleTestDatabase oracleTestDatabase = new OracleTestDatabase();
+      params.add(new Object[] {"integration/config/oracle-db-config.xml", oracleTestDatabase,
+          oracleTestDatabase.getDbType(), emptyList()});
+    }
+
+    return params;
+  }
+
+  @Override
+  protected String[] getFlowConfigurationResources() {
+    return new String[] {"integration/storedprocedure/stored-procedure-oracle-with-blob-output.xml"};
+  }
+
+  @Test
+  public void verifyConnectionIsReleasedOutputBlob() throws Exception {
+    flowRunner("getBlobOutputFromStoredProcedurePackage").run();
+    flowRunner("getBlobOutputFromStoredProcedurePackage").run();
+  }
+
+  @Before
+  public void setupStoredProcedure() throws Exception {
+    testDatabase.createStoredProcedureOutputBlob(getDefaultDataSource());
+  }
+}

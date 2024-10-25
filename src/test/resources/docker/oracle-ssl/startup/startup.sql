2daf690602ab784d33a49707115c94d7eb80af3c
Cristian Chan <cristian.chan@mulesoft.com>
2024-10-25 11:21:12 -0700

    [W-16615688] refactor docker image

--- src/test/resources/docker/oracle-ssl/startup/startup.sql
+++
@@ -1,6 +0,0 @@
--- Ignore pluggable databases
-alter session set "_ORACLE_SCRIPT"=true;  
-  
--- Add authenticated user
-create user sslclient identified externally as'CN=localhost';
-grant connect,create session to sslclient;
